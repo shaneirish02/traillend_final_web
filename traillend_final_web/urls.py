50ca7a920c85dd86d6ec3b002b25de7657d08522
shaneirish02 <160092469+shaneirish02@users.noreply.github.com>
2025-11-27 20:36:14 +0800

    Update urls.py

--- traillend_final_web/urls.py
+++ traillend_final_web/urls.py
@@ -4,12 +4,9 @@ from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import apply_migrations
-from django.contrib.staticfiles.views import serve
-from django.views.decorators.cache import never_cache
 
 urlpatterns = [
     path("apply-migrations/", apply_migrations),
-    path("static-test/<path:path>", never_cache(serve))
     path("admin/", admin.site.urls),
     path("", lambda request: redirect("login")),  # redirect root to login page
     path("", include('core.urls')),
