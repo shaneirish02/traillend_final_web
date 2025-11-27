859d6607caba54737f9e0b9e30d4769014c7d7a2
shaneirish02 <160092469+shaneirish02@users.noreply.github.com>
2025-11-27 19:21:28 +0800

    Update settings.py

--- traillend_final_web/settings.py
+++ traillend_final_web/settings.py
@@ -202,7 +202,10 @@ USE_TZ = True
 # ============================================
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / "core" / "static"
+]
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
